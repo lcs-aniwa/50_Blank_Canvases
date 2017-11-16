1969700d7afadb3ad467916d47140920ddfa98c9
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2017-11-16 10:08:16 -0500

    Using a function

--- 50 Blank Canvases.playground/Pages/Hexagon.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Hexagon.xcplaygroundpage/Contents.swift
@@ -1,19 +1,18 @@
 //: [Previous](@previous) / [Next](@next)
-//: # A Blank Canvas
-//:
 //: Use this page to experiment. Have fun!
-/*:
- ## Required code
+// ## Required code
  
- The following statements are required to make the playground run.
+ //The following statements are required to make the playground run.
  
- Please do not remove.
- */
+// Please do not remove.*/
 import Cocoa
 import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 400, height: 300)
+
+
+
 
 /*:
  ## Add your code below
--- 50 Blank Canvases.playground/Pages/Pentagon.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Pentagon.xcplaygroundpage/Contents.swift
@@ -1,19 +1,30 @@
 //: [Previous](@previous) / [Next](@next)
-//: # A Blank Canvas
-//:
 //: Use this page to experiment. Have fun!
-/*:
- ## Required code
+// ## Required code
  
- The following statements are required to make the playground run.
+// The following statements are required to make the playground run.
  
- Please do not remove.
- */
+// Please do not remove.
+// */
 import Cocoa
 import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 400, height: 300)
+
+
+//  Step 1: Move to the starting point
+canvas.translate(byX: 75, byY: 100)
+canvas.drawAxes()
+canvas.defaultLineWidth = 5
+
+//Use a loop to draw four sides
+for _ in 1..3 { //loops four times creates no variable
+    canvas.drawLine(fromX: 0, fromY: 0, toX: 200, toY: 0)
+    canvas.translate(byX: 200, byY: 0)
+    canvas.rotate(by: 108)
+    
+}
 
 /*:
  ## Add your code below
