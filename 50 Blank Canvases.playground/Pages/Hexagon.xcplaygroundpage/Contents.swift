2df74a97f113ea09661106bae635ad4c437220d9
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2017-11-16 12:43:08 -0500

    Hexagon

--- 50 Blank Canvases.playground/Pages/Hexagon.xcplaygroundpage/Contents.swift
+++ 50 Blank Canvases.playground/Pages/Hexagon.xcplaygroundpage/Contents.swift
@@ -25,24 +25,17 @@ let canvas = Canvas(width: 400, height: 300)
 
 
 
-/*:
- ## Use source control
- To keep your work organized, and receive feedback, source control is a must.
- 
- Please commit and push your work often.
- 
- ![source_control](source_control.png "Source Control")
- */
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right.
- 
- Please do not remove.
- 
- If you don't see output, remember to show the Assistant Editor, and switch to Live View:
- 
- ![timeline](timeline.png "Timeline")
- */
-// Don't remove the code below
+//  Step 1: Move to the starting point
+canvas.translate(byX: 75, byY: 100)
+canvas.drawAxes()
+canvas.defaultLineWidth = 5
+
+//Use a loop to draw four sides
+for _ in 1...6 { //loops five times creates no variable
+    canvas.drawLine(fromX: 0, fromY: 0, toX: 100, toY: 0)
+    canvas.translate(byX: 100, byY: 0)
+    canvas.rotate(by: 60)
+    
+}
 PlaygroundPage.current.liveView = canvas.imageView
 
